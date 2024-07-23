b4a4ba8fb2c4259e7282ee70258d10c4e49fa860
minhchauU23 <chauminhtran2@gmail.com>
2024-07-23 14:38:17 +0700

    fix: word controller

--- src/main/java/com/example/appdictionaryghtk/controller/WordController.java
+++ src/main/java/com/example/appdictionaryghtk/controller/WordController.java
@@ -1,7 +1,7 @@
 package com.example.appdictionaryghtk.controller;
 
 import com.example.appdictionaryghtk.dtos.word_management.word.WordDetail;
-import com.example.appdictionaryghtk.service.word.WordServiceImpl;
+import com.example.appdictionaryghtk.service.word.WordService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -14,31 +14,31 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class WordController {
-    WordServiceImpl wordServiceImpl;
+    WordService wordService;
 
     @GetMapping("/words/{pageNumber}/{pageSize}")
     public Page<WordDetail> findAll(@PathVariable Integer pageNumber, @PathVariable Integer pageSize){
-        return wordServiceImpl.findAll(pageNumber, pageSize, null);
+        return wordService.findAll(pageNumber, pageSize, null);
     }
 
     @GetMapping("/words/{id}")
     public ResponseEntity<WordDetail> getWord(@PathVariable("id") Integer id){
-        return ResponseEntity.ok(wordServiceImpl.findByID(id));
+        return ResponseEntity.ok(wordService.findByID(id));
     }
 
     @PostMapping("/words")
     public ResponseEntity<WordDetail> create(@RequestBody WordDetail wordDetail){
-        return ResponseEntity.ok(wordServiceImpl.create(wordDetail));
+        return ResponseEntity.ok(wordService.create(wordDetail));
     }
 
     @PutMapping("/words/{wordID}")
     public ResponseEntity<WordDetail> update(@PathVariable("wordID") Integer wordID, @RequestBody WordDetail wordDetail){
-        return ResponseEntity.ok(wordServiceImpl.update(wordID, wordDetail));
+        return ResponseEntity.ok(wordService.update(wordID, wordDetail));
     }
 
     @DeleteMapping("/words/{id}")
     public ResponseEntity<?> deleteWord(@PathVariable("id") Integer id){
-        wordServiceImpl.deleteByID(id);
+        wordService.deleteByID(id);
         return ResponseEntity.ok("Delete Successful");
     }
 
