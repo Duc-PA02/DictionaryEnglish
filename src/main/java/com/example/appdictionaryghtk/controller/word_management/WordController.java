35ab9b87b519f850308ea407d04ae689deba9dd3
minhchauU23 <chauminhtran2@gmail.com>
2024-08-06 14:15:19 +0700

    fix: word

--- src/main/java/com/example/appdictionaryghtk/controller/word_management/WordController.java
+++ src/main/java/com/example/appdictionaryghtk/controller/word_management/WordController.java
@@ -1,5 +1,6 @@
 package com.example.appdictionaryghtk.controller.word_management;
 
+import com.example.appdictionaryghtk.dtos.word_management.word.WordDTO;
 import com.example.appdictionaryghtk.dtos.word_management.word.WordDetail;
 import com.example.appdictionaryghtk.service.word_management.WordService;
 import lombok.AccessLevel;
@@ -9,17 +10,34 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/api")
+import java.util.List;
+
+
+@RestController("wordManagementController")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class WordController {
     WordService wordService;
 
-    @GetMapping("/words/{pageNumber}/{pageSize}")
-    public Page<WordDetail> findAll(@PathVariable Integer pageNumber, @PathVariable Integer pageSize){
-        return wordService.findAll(pageNumber, pageSize, null);
+    @GetMapping("/words/after")
+    public List<WordDTO> getWordsAfter(@RequestParam(required = false, defaultValue ="0") Integer lastId,
+                                    @RequestParam(defaultValue = "10") int limit) {
+        return wordService.getWordsAfter(lastId, limit);
     }
+
+    @GetMapping("/words/before")
+    public List<WordDTO> getWordsBefore(@RequestParam(required = false) Integer firstId,
+                                        @RequestParam(defaultValue = "10") int limit) {
+        return wordService.getWordsBefore(firstId, limit);
+    }
+
+    @GetMapping("/words/page")
+    public Page<WordDTO> getWordsByPage(@RequestParam(defaultValue = "0") int page,
+                                     @RequestParam(defaultValue = "10") int limit) {
+        return wordService.getWordsByPage(page, limit);
+    }
+
 
     @GetMapping("/words/{id}")
     public ResponseEntity<WordDetail> getWord(@PathVariable("id") Integer id){
--- src/main/java/com/example/appdictionaryghtk/service/word_management/WordService.java
+++ src/main/java/com/example/appdictionaryghtk/service/word_management/WordService.java
@@ -1,17 +1,27 @@
 package com.example.appdictionaryghtk.service.word_management;
 
+import com.example.appdictionaryghtk.dtos.response.word.AntonymDTO;
+import com.example.appdictionaryghtk.dtos.response.word.SynonymDTO;
 import com.example.appdictionaryghtk.dtos.word_management.type.TypeDTO;
+import com.example.appdictionaryghtk.dtos.word_management.word.WordDTO;
 import com.example.appdictionaryghtk.dtos.word_management.word.WordDetail;
+import com.example.appdictionaryghtk.entity.Antonyms;
+import com.example.appdictionaryghtk.entity.Synonyms;
 import com.example.appdictionaryghtk.entity.Type;
 import com.example.appdictionaryghtk.entity.Word;
 import com.example.appdictionaryghtk.exceptions.MissingPropertyException;
 import com.example.appdictionaryghtk.repository.WordRepository;
+import com.example.appdictionaryghtk.service.antonym.AntonymService;
+import com.example.appdictionaryghtk.service.synonym.SynonymService;
 import com.example.appdictionaryghtk.service.type.TypeService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.modelmapper.ModelMapper;
-import org.springframework.data.domain.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,38 +37,66 @@ public class WordService implements IWordService {
 
     WordRepository wordRepository;
     TypeService typeServiceImpl;
+    AntonymService antonymService;
+    SynonymService synonymService;
     ModelMapper mapper;
 
-    @Override
-    public Page<WordDetail> findAll(Integer pageNumber, Integer pageSize, String sort) {
-        Pageable pageable = null;
-        if (sort != null) {
-            pageable = PageRequest.of(pageNumber-1, pageSize, Sort.Direction.ASC, sort);
-        } else {
-            pageable = PageRequest.of(pageNumber-1, pageSize);
-        }
-        Page<Word> words = wordRepository.findAll(pageable);
-        return new PageImpl<>(
-                words.stream().map(
-                        word->mapper.map(word, WordDetail.class)
-                ).toList(), pageable, words.getTotalElements());
+    public List<WordDTO> getWordsAfter(Integer lastId, int pageSize) {
+        return wordRepository.nextPage(lastId, pageSize)
+                .stream().map(word -> mapper.map(word,WordDTO.class)).toList();
     }
+
+    public List<WordDTO> getWordsBefore(Integer firstId, int pageSize) {
+        List<Word> words = wordRepository.previousPage(firstId, pageSize);
+        return words.stream().map(word -> mapper.map(word, WordDTO.class)).toList();
+    }
+
+    public Page<WordDTO> getWordsByPage(int page, int pageSize) {
+        Pageable pageable = PageRequest.of(page, pageSize, Sort.by("id").ascending());
+        Page<Word> wordPage = wordRepository.findAll(pageable);
+        return wordPage.map(word -> mapper.map(word, WordDTO.class));
+    }
+
 
     @Override
     public WordDetail findByID(Integer wordID) {
         Word word =  wordRepository.findById(wordID).orElseThrow(()->new RuntimeException("Word is not exist"));
-        return mapper.map(word, WordDetail.class);
+        WordDetail wordDetail = new WordDetail();
+        wordDetail.setName(word.getName());
+        wordDetail.setId(word.getId());
+        wordDetail.setTypeList(
+                word.getTypeList().stream().map(
+                        type -> mapper.map(type, TypeDTO.class)
+                ).toList()
+        );
+        wordDetail.setAntonymsList(
+                word.getAntonymsList().stream().map(
+                        antonyms -> mapper.map(antonyms, AntonymDTO.class)
+                ).toList()
+
+        );
+        wordDetail.setSynonymsList(
+                word.getSynonymsList().stream().map(
+                        synonyms -> mapper.map(synonyms, SynonymDTO.class)
+                ).toList()
+
+        );
+        return wordDetail;
     }
 
     @Override
     @Transactional
     public WordDetail create(WordDetail wordDetail) {
-        if(wordRepository.existsByName(wordDetail.getName())) throw new RuntimeException("Duplycate for word name");
+        if(wordRepository.existsByName(wordDetail.getName())) throw new RuntimeException("Duplicate for word name");
         if(wordDetail.getTypeList().size() <= 0) throw new MissingPropertyException(("Can it nhat 1 type"));
         Word word = mapper.map(wordDetail, Word.class);
         word.setTypeList(new ArrayList<>());
+        word.setAntonymsList(new ArrayList<>());
+        word.setSynonymsList(new ArrayList<>());
         wordRepository.save(word);
         addTypeOfWord(wordDetail.getTypeList(), word);
+        addAntonymOfWord(wordDetail.getAntonymsList(), word);
+        addSynonymOfWord(wordDetail.getSynonymsList(), word);
         return mapper.map(word, WordDetail.class);
     }
 
@@ -66,6 +104,8 @@ public class WordService implements IWordService {
     public void deleteByID(Integer id) {
         wordRepository.deleteById(id);
     }
+
+
 
     @Override
     @Transactional
@@ -93,16 +133,58 @@ public class WordService implements IWordService {
         updateTypeOfWord( updateTypes, existedWord);
         deleteTypeOfWord( updateTypes, deleteTypes, existedWord);
         addTypeOfWord(nonExistTypes, existedWord);
+
+        List<AntonymDTO> nonExistAntonym = wordDetail.getAntonymsList()
+                .stream()
+                .filter(antonymDTO -> antonymDTO.getId()==null)
+                .toList();
+
+        Map<Integer, AntonymDTO> savedAntonym  = wordDetail.getAntonymsList()
+                .stream()
+                .filter(antonymDTO -> antonymDTO.getId()!=null)
+                .collect(Collectors.toMap(AntonymDTO::getId, antonymDTO -> antonymDTO));
+
+
+        List<AntonymDTO> deleteAntonym = new ArrayList<>();
+        for(Antonyms antonyms: existedWord.getAntonymsList()){
+            if(!savedAntonym.containsKey(antonyms.getId())){
+                deleteAntonym.add(mapper.map(antonyms, AntonymDTO.class));
+            }
+        }
+        deleteAntonymOfWord(savedAntonym,deleteAntonym, existedWord);
+        addAntonymOfWord(nonExistAntonym, existedWord);
+
+
+
+        List<SynonymDTO> nonExistSynonym = wordDetail.getSynonymsList()
+                .stream()
+                .filter(synonymDTO -> synonymDTO.getId()==null)
+                .toList();
+
+        Map<Integer, SynonymDTO> savedSynonym  = wordDetail.getSynonymsList()
+                .stream()
+                .filter(synonymDTO -> synonymDTO.getId()!=null)
+                .collect(Collectors.toMap(SynonymDTO::getId, synonymDTO -> synonymDTO));
+
+
+        List<SynonymDTO> deleteSynonyms = new ArrayList<>();
+        for(Synonyms synonyms: existedWord.getSynonymsList()){
+            if(!savedSynonym.containsKey(synonyms.getId())){
+                deleteSynonyms.add(mapper.map(synonyms, SynonymDTO.class));
+            }
+        }
+        deleteSynonymOfWord(savedSynonym,deleteSynonyms, existedWord);
+        addSynonymOfWord(nonExistSynonym, existedWord);
+
         return mapper.map(wordRepository.save(existedWord), WordDetail.class);
     }
 
-    @Transactional
-    private void addTypeOfWord(List<TypeDTO> nonExistTypes, Word existedWord){
+    void addTypeOfWord(List<TypeDTO> nonExistTypes, Word existedWord){
         for(TypeDTO type : nonExistTypes){
             typeServiceImpl.create(existedWord.getId(), type);
         }
     }
-    @Transactional
+
     private void updateTypeOfWord(Map<Integer, TypeDTO> updateTypes, Word existedWord){
         for(Type type: existedWord.getTypeList()){
             if(updateTypes.containsKey(type.getId())){
@@ -111,7 +193,7 @@ public class WordService implements IWordService {
             }
         }
     }
-    @Transactional
+
     private void deleteTypeOfWord(Map<Integer, TypeDTO> updateTypes, List<TypeDTO> deleteTypes, Word existedWord ){
         if(existedWord.getTypeList().size() - deleteTypes.size() <=0 )
             throw new MissingPropertyException("Phai ton tai it nhat 1 type trong word");
@@ -122,5 +204,35 @@ public class WordService implements IWordService {
 
     }
 
+    private void addAntonymOfWord(List<AntonymDTO> nonExistAntonyms, Word existedWord){
+        for(AntonymDTO antonym : nonExistAntonyms){
+            antonymService.create(existedWord.getId(), antonym);
+            Antonyms antonyms = new Antonyms();
+            antonyms.setAntonym(existedWord);
+//            antonymService.create(antonym.getAntonym().getId(), mapper.map(antonyms, AntonymDTO.class));
+        }
+    }
+
+    private void deleteAntonymOfWord(Map<Integer, AntonymDTO> savedAntonym,
+                                     List<AntonymDTO> deleteAntonym, Word existedWord){
+        existedWord.getAntonymsList().removeIf(antonym -> !savedAntonym.containsKey(antonym.getId()));
+        for(AntonymDTO antonymDTO: deleteAntonym){
+            antonymService.deleteByID(antonymDTO.getId());
+        }
+    }
+
+    private void addSynonymOfWord(List<SynonymDTO> nonExistSynonyms, Word existedWord){
+        for(SynonymDTO synonym : nonExistSynonyms){
+            synonymService.create(existedWord.getId(), synonym);
+        }
+    }
+
+    private void deleteSynonymOfWord(Map<Integer, SynonymDTO> savedSynonym,
+                                     List<SynonymDTO> deleteSynonym, Word existedWord){
+        existedWord.getSynonymsList().removeIf(synonym -> !savedSynonym.containsKey(synonym.getId()));
+        for(SynonymDTO synonymDTO: deleteSynonym){
+            synonymService.deleteByID(synonymDTO.getId());
+        }
+    }
 
 }
