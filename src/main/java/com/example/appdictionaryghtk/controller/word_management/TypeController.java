6fb17a49d66a461eda13eab322a6fcaa8cbdee48
minhchauU23 <chauminhtran2@gmail.com>
2024-08-26 21:14:20 +0700

    fix: dtos

--- src/main/java/com/example/appdictionaryghtk/controller/word_management/TypeController.java
+++ src/main/java/com/example/appdictionaryghtk/controller/word_management/TypeController.java
@@ -1,25 +1,32 @@
 package com.example.appdictionaryghtk.controller.word_management;
 
+import com.example.appdictionaryghtk.dtos.response.ResponseObject;
 import com.example.appdictionaryghtk.dtos.word_management.type.TypeDTO;
 import com.example.appdictionaryghtk.service.type.TypeService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-@Controller
+@RestController
 @RequestMapping("${api.prefix}/admin")
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @RequiredArgsConstructor
 public class TypeController {
     TypeService typeService;
     @GetMapping("/types")
-    public ResponseEntity<List<TypeDTO>> findAllType(){
-        return ResponseEntity.ok(typeService.findAllTypeName());
+    public ResponseObject findAllType(){
+        return ResponseObject.builder()
+                .status(HttpStatus.OK)
+                .message("Success")
+                .data(typeService.findAllTypeName())
+                .build();
     }
 }
